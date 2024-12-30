c991087a0f2fe89589edd12132d327399549b56f
BogdanChep <kursrabota33ikb@gmail.com>
2024-12-31 02:32:54 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module Game {
-    requires java.desktop; // Для работы с Java Swing
-    requires org.jfree.jfreechart; // Зависимость для JFreeChart
-    requires org.apache.logging.log4j; // Зависимость для Log4j API
-    requires org.apache.logging.log4j.core;
-
-    exports Game; // Экспорт пакетa Game, чтобы другие модули могли его использовать
-}
